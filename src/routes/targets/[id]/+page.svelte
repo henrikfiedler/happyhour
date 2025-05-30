ce67f252fae80aafce4e519edc6416837022b209
Henrik Fiedler <33574047+henrikfiedler@users.noreply.github.com>
2025-05-30 23:34:58 +0200

    basic area chart

--- src/routes/targets/[id]/+page.svelte
+++ src/routes/targets/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AreaChart from '$lib/components/charts/AreaChart.svelte';
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
@@ -7,3 +8,6 @@
 <p>
 	Start: {data.target?.startDate.toLocaleDateString(undefined, { dateStyle: 'medium' })}
 </p>
+
+
+<AreaChart></AreaChart>
