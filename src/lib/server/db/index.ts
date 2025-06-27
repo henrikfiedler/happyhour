1a1e9d27dfea250166094399e2fd86ff694837bb
Henrik Fiedler <33574047+henrikfiedler@users.noreply.github.com>
2025-06-28 00:20:16 +0200

    feat: Add Neon database support and update dependencies

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,14 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle as drizzlePg } from 'drizzle-orm/postgres-js';
+import { drizzle } from 'drizzle-orm/neon-http';
 import postgres from 'postgres';
+import { neon } from "@neondatabase/serverless";
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(env.DATABASE_URL);
+/* const pgClient = postgres(env.DATABASE_URL)
+const client = neon(env.DATABASE_URL); */
 
-export const db = drizzle(client, { schema });
+export const db = dev ? drizzlePg({ client: postgres(env.DATABASE_URL), schema }) : drizzle({ client: neon(env.DATABASE_URL), schema });
