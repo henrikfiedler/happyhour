1c483fbfc585458ff4a98fb1bcb13eff0029325e
Henrik Fiedler <33574047+henrikfiedler@users.noreply.github.com>
2025-06-28 11:46:07 +0200

    feat: Remove secure option from SMTP transport configuration

--- src/lib/server/email/index.ts
+++ src/lib/server/email/index.ts
@@ -1,22 +1,10 @@
-import { PRIVATE_MAIL_HOST, PRIVATE_MAIL_PASS, PRIVATE_MAIL_PORT, PRIVATE_MAIL_SECURE, PRIVATE_MAIL_USER } from '$env/static/private'
+import { PRIVATE_MAIL_HOST, PRIVATE_MAIL_PASS, PRIVATE_MAIL_PORT, PRIVATE_MAIL_USER } from '$env/static/private'
 import type { User } from '$lib/types';
 import SMTPTransport from 'nodemailer'
-
-const options = {
-    host: PRIVATE_MAIL_HOST,
-    port: Number(PRIVATE_MAIL_PORT),
-    secure: Number(PRIVATE_MAIL_SECURE) === 1,
-    auth: {
-        user: PRIVATE_MAIL_USER,
-        pass: PRIVATE_MAIL_PASS
-    }
-}
-console.log("🚀 ~ options:", options)
 
 const transporter = SMTPTransport.createTransport({
     host: PRIVATE_MAIL_HOST,
     port: Number(PRIVATE_MAIL_PORT),
-    // secure: Number(PRIVATE_MAIL_SECURE) === 1,
     auth: {
         user: PRIVATE_MAIL_USER,
         pass: PRIVATE_MAIL_PASS
