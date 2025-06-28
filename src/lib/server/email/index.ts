c6d69130c1646f5655217aa8be8d82478bbe4a92
Henrik Fiedler <33574047+henrikfiedler@users.noreply.github.com>
2025-06-28 11:09:09 +0200

    feat: Refactor email transport options and update sender format in sendMail function

--- src/lib/server/email/index.ts
+++ src/lib/server/email/index.ts
@@ -2,6 +2,17 @@ import { PRIVATE_MAIL_HOST, PRIVATE_MAIL_PASS, PRIVATE_MAIL_PORT, PRIVATE_MAIL_S
 import type { User } from '$lib/types';
 import SMTPTransport from 'nodemailer'
 
+const options = {
+    host: PRIVATE_MAIL_HOST,
+    port: Number(PRIVATE_MAIL_PORT),
+    secure: Number(PRIVATE_MAIL_SECURE) === 1,
+    auth: {
+        user: PRIVATE_MAIL_USER,
+        pass: PRIVATE_MAIL_PASS
+    }
+}
+console.log("🚀 ~ options:", options)
+
 const transporter = SMTPTransport.createTransport({
     host: PRIVATE_MAIL_HOST,
     port: Number(PRIVATE_MAIL_PORT),
@@ -15,7 +26,7 @@ const transporter = SMTPTransport.createTransport({
 
 export async function sendMail({ to, subject, body }: { to: User['email'], subject: string, body: string }) {
     await transporter.sendMail({
-        from: PRIVATE_MAIL_USER,
+        from: `"HappyHour" <${PRIVATE_MAIL_USER}>`,
         to,
         subject,
         html: body
